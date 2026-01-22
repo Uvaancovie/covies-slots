897b2e99212d9339b5799f0476b5aef07c4b4f4e
GVNDAN011 <ST10022006@VCCONNECT.EDU.ZA>
2026-01-22 14:59:36 +0200

    the fix

--- api-handlers/deposit.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import walletHandler from '../wallet';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (!req.query) req.query = {} as any;
-  (req.query as any).action = 'deposit';
-  return walletHandler(req, res);
-}
--- api-handlers/history-analysis.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import historyHandler from './history';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (!req.query) req.query = {} as any;
-  (req.query as any).action = 'analysis';
-  return historyHandler(req, res);
-}
